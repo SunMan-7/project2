2aa4972fe66ac512a551a8116187be5fcd83d40a
root <root@lil-cub.>
2025-05-26 16:34:19 -0600

    change

---
+++ nhost/migrations/default/1748298797292_alter_table_public_images/down.sql
@@ -0,0 +1,3 @@
+-- Could not auto-generate a down migration
+-- Please write an appropriate down migration for the SQL below:
+-- ALTER TABLE public.images DROP CONSTRAINT IF EXISTS images_deployment_id_fkey;
---
+++ nhost/migrations/default/1748298797292_alter_table_public_images/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE public.images DROP CONSTRAINT IF EXISTS images_deployment_id_fkey;
